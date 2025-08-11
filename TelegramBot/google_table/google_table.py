5a5286d760f48759bae16cc4c8a9a980f7ab8851
Evgeny Irikov <Evgen5060@gmail.com>
2025-08-11 11:27:17 +0500

    refactor(project): рефакторинг проекта

--- TelegramBot/google_table/google_table.py
+++ TelegramBot/google_table/google_table.py
@@ -1,309 +1,238 @@
-from config.bot_config import GOOGLE_URL, IGNORE_DATE
+from config.bot_config import GOOGLE_URL, IGNORE_EVENTS
 import pygsheets
 import datetime
+from typing import List, Tuple, Union
 
 class GoogleTable:
-    
     def __init__(
-        self, credence_service_file:str = "config/google_config.json", googlesheet_file_url:str = GOOGLE_URL
+        self, 
+        credence_service_file: str = "config/google_config.json", 
+        googlesheet_file_url: str = GOOGLE_URL
     ) -> None:
         self.credence_service_file = credence_service_file
         self.googlesheet_file_url = googlesheet_file_url
+        self._client = None  # Кэширование клиента
 
-    def _get_googlesheet_by_url(
-        self, googlesheet_client: pygsheets.client.Client, sheet_name:str
-    ) -> pygsheets.Spreadsheet:
-        sheets: pygsheets.Spreadsheet = googlesheet_client.open_by_url(
-            self.googlesheet_file_url
-        )
-        return sheets.worksheet_by_title(sheet_name)
+    @property
+    def client(self) -> pygsheets.client.Client:
+        """Кэшированный клиент Google Sheets."""
+        if self._client is None:
+            self._client = pygsheets.authorize(
+                service_file=self.credence_service_file
+            )
+        return self._client
 
-    def _get_googlesheet_client(self):
-        return pygsheets.authorize(
-            service_file=self.credence_service_file
-        )
+    def _get_worksheet(self, sheet_name: str) -> pygsheets.Worksheet:
+        """Получает рабочий лист по имени."""
+        spreadsheet = self.client.open_by_url(self.googlesheet_file_url)
+        return spreadsheet.worksheet_by_title(sheet_name)
 
-#  - - - - - - - - - - - - - Вспомогательные функции - - - - - - - - - - - - - - - - #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # ----------------- Вспомогательные функции ----------------- #
 
-    # Удобное форматирование ячейки
-    def Cell(self, column:str, row):
-        return column + str(row)
-    
-    # Количество квартир
-    def get_k_flats(self):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo")  
+    @staticmethod
+    def format_cell(column: str, row: int) -> str:
+        """Форматирует адрес ячейки."""
+        return f"{column}{row}"
 
-        cell = self.Cell("A", 2)
-        k_flats = int(wks.get_value(cell))
+    def get_k_flats(self) -> int:
+        """Возвращает количество квартир."""
+        wks = self._get_worksheet("botInfo")
+        return int(wks.get_value(self.format_cell("A", 2)))
 
-        return k_flats    
-    
-    # Количество пользователей
-    def get_k_users(self):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo")  
+    def get_k_users(self) -> int:
+        """Возвращает количество пользователей."""
+        wks = self._get_worksheet("botInfo")
+        return int(wks.get_value(self.format_cell("A", 3)))
 
-        cell = self.Cell("A", 3)
-        k_users = int(wks.get_value(cell))
+    def get_row_communal(self, sheet_name: str) -> int:
+        """Возвращает текущую строку для показаний."""
+        wks = self._get_worksheet(sheet_name)
+        return int(wks.get_value(self.format_cell("J", 1)))
 
-        return k_users 
+    # ----------------- Функции для баз данных ----------------- #
 
-    # Номер строки в таблице счетчиков
-    def get_row_communal(self, sheet_name:str):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, sheet_name)  
-
-        cell = self.Cell("J", 1)
-        row = int(wks.get_value(cell))
-
-        return row 
-
-
-#  - - - - - - - - - - - - - Фунции для баз данных  - - - - - - - - - - - - - - - - #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-    # Считывание квартир для ввода в БД
-    def get_flats_db(self):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo")       
-
+    def get_flats_db(self) -> List[list]:
+        """Получает данные о квартирах для БД."""
+        wks = self._get_worksheet("botInfo")
         k_flats = self.get_k_flats()
+        
+        start = self.format_cell("C", 2)
+        end = self.format_cell("E", k_flats + 1)
+        
+        return wks.get_values(start, end)
 
-        cell_start = self.Cell("C", 2)
-        cell_end = self.Cell("E", k_flats + 1)
-
-        lst = wks.get_values(cell_start, cell_end)
-
-        return lst
-    
-    # Считывание пользователей для ввода в БД
-    def get_users_db(self):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo")       
-
+    def get_users_db(self) -> List[list]:
+        """Получает данные о пользователях для БД."""
+        wks = self._get_worksheet("botInfo")
         k_users = self.get_k_users()
-
-        cell_start = self.Cell("F", 2)
-        cell_end = self.Cell("G", k_users + 1)
-
+        
+        start = self.format_cell("F", 2)
+        end = self.format_cell("G", k_users + 1)
+        
         try:
-            lst = wks.get_values(cell_start, cell_end)
-        except:
-            lst = []
+            return wks.get_values(start, end)
+        except Exception:
+            return []
 
-        return lst
-    
+    # ----------------- Функции показаний счетчиков ----------------- #
 
-#  - - - - - - - - - - - - - Функции показаний счетчиков  - - - - - - - - - - - - - #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    def get_tariffs(self) -> List[float]:
+        """Возвращает текущие тарифы ЖКХ."""
+        wks = self._get_worksheet("botInfo")
+        
+        start = self.format_cell("B", 10)
+        end = self.format_cell("B", 13)
+        
+        values = wks.get_values(start, end)
+        return [float(v[0].replace(',', '.')) for v in values]
 
-    # Тарифы ЖКХ
-    def get_tariffs(self):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo")      
-
-        cell_start = self.Cell("B", 10)
-        cell_end = self.Cell("B", 13)
-
-        lst = wks.get_values(cell_start, cell_end)
-
-        # Форматирование значений из таблицы
-        lst_communal = []
-        for i in lst:
-            for j in i:
-                lst_communal.append(float(j.replace(',', '.')))
-
-        return lst_communal
-
-    # МОЖНО СДЕЛАТЬ АСИНХРОННОЙ?
-    # Запись новых показаний счетчиков в таблицу
-    def set_new_communal(self, sheet_name:str, meters):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, sheet_name) 
-
-        # Сегодняшняя дата
-        current_time = datetime.datetime.now()
-        date = f"{str(current_time.day).zfill(2)}.{str(current_time.month).zfill(2)}.{str(current_time.year)[2:]}"
+    def set_new_communal(self, sheet_name: str, meters: list) -> None:
+        """Записывает новые показания счетчиков."""
+        wks = self._get_worksheet(sheet_name)
         row = self.get_row_communal(sheet_name) + 1
+        
+        # Форматирование даты
+        now = datetime.datetime.now()
+        date_str = now.strftime("%d.%m.%y")
+        
+        # Обновление значений за один запрос
+        update_data = [
+            (f"A{row}", date_str),
+            (f"B{row}", meters[0]),
+            (f"G{row}", meters[1]),
+            (f"L{row}", meters[2])
+        ]
+        
+        for cell, value in update_data:
+            wks.cell(cell).set_value(value)
 
-        cell_date = wks.cell(f"A{row}")
-        cell_date.set_value(date)
-
-        cell_electro = wks.cell(f"B{row}")
-        cell_electro.set_value(meters[0])
-
-        cell_cold = wks.cell(f"G{row}")
-        cell_cold.set_value(meters[1])
-
-        cell_hot = wks.cell(f"L{row}")
-        cell_hot.set_value(meters[2])
-
-        return
-
-    def get_log(self, sheet_name:str, row_start = -1):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, sheet_name) 
-
-        row = self.get_row_communal(sheet_name)
-
-        if row_start == -1:
-            cell_start = self.Cell("A", 10)     
-        else:    
-            cell_start = self.Cell("A", row - row_start)
-        cell_end = self.Cell("T", row)
-
-        lst = wks.get_values(cell_start, cell_end)
-
-        # Форматируем данные
-        for i in range(len(lst)):
-            for j in range(20):
-                lst[i][j] = lst[i][j].replace(',', '.')
-
-        log = [[] * 6 for i in range(len(lst))]
-
-        for i in range(len (lst)):
-
-            if (lst[i][0].lower() in IGNORE_DATE):
-                log[i] = [lst[i][0].lower(), 0, 0, 0, 0]
+    def get_log(self, sheet_name: str, row_start: int = -1) -> List[list]:
+        """Получает журнал показаний."""
+        wks = self._get_worksheet(sheet_name)
+        current_row = self.get_row_communal(sheet_name)
+        
+        start_row = 10 if row_start == -1 else current_row - row_start
+        end_row = current_row
+        
+        start = self.format_cell("A", start_row)
+        end = self.format_cell("T", end_row)
+        
+        data = wks.get_values(start, end)
+        log = []
+        
+        for row in data:
+            # Обработка специальных событий
+            if row[0].lower() in IGNORE_EVENTS:
+                log.append([row[0].lower(), 0, 0, 0, 0])
                 continue
-
-            if(len(lst[i][0]) > 5 ):
-                date = lst[i][0]
-                lst[i][0] = date[:5]
-
-            log[i].append(lst[i][0])
-            log[i].append(int(lst[i][1]))
-            log[i].append(int(lst[i][6]))
-            log[i].append(int(lst[i][11]))
-            log[i].append(float(lst[i][19]))
-
+            
+            # Форматирование даты
+            date_str = row[0][:5] if len(row[0]) > 5 else row[0]
+            
+            # Извлечение значений
+            try:
+                electro = int(row[1]) if row[1] else 0
+                cold = int(row[6]) if row[6] else 0
+                hot = int(row[11]) if row[11] else 0
+                total = float(row[19].replace(',', '.')) if row[19] else 0.0
+            except (ValueError, TypeError):
+                electro = cold = hot = total = 0
+                
+            log.append([date_str, electro, cold, hot, total])
+        
         return log
 
-#  - - - - - - - - - -  Различная информация о квартире - - - - - - - - - - - - - - #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # ----------------- Информация о квартире ----------------- #
 
-    # Информация о квартире
-    def get_info(self, sheet_name:str):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, sheet_name) 
+    def get_info(self, sheet_name: str) -> List[list]:
+        """Получает информацию о квартире."""
+        wks = self._get_worksheet(sheet_name)
+        start = self.format_cell("V", 2)
+        end = self.format_cell("W", 7)
+        return wks.get_values(start, end)
 
-        cell_start = self.Cell("V", 2)
-        cell_end = self.Cell("W", 7)
+    def get_equip(self, sheet_name: str) -> List[str]:
+        """Получает список оборудования."""
+        wks = self._get_worksheet(sheet_name)
+        
+        # Получаем количество позиций оборудования
+        try:
+            row_equip = int(wks.get_value("W8"))
+        except (ValueError, TypeError):
+            row_equip = 0
+        
+        if row_equip <= 0:
+            return []
+        
+        start = self.format_cell("Y", 2)
+        end = self.format_cell("Y", row_equip + 1)
+        
+        values = wks.get_values(start, end)
+        return [item[0] for item in values] if values else []
 
-        lst = wks.get_values(cell_start, cell_end)
+    # ----------------- Операции с пользователями ----------------- #
 
-        return lst
-
-    # Список оборудования
-    def get_equip(self, sheet_name:str):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, sheet_name) 
-
-        row_equip = int(wks.get_value("W8"))
-
-        cell_start = self.Cell("Y", 2)
-        cell_end = self.Cell("Y", row_equip + 1)
-
-        lst = wks.get_values(cell_start, cell_end)     
-
-        # Форматирование данных
-        equip = []
-        for i in lst:
-            for j in i:
-                equip.append(j)
-
-        return equip    
-
-
-#  - - - - - - - - - -  Функции Inline клавиатур  - - - - - - - - - - - - - - - - - #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-    # МОЖНО СДЕЛАТЬ АСИНХРОННОЙ?
-    # Запись нового пользователя в таблицу
-    def login_agree(self, user_id, flat):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo") 
-
+    def login_agree(self, user_id: Union[int, str], flat: str) -> None:
+        """Регистрирует нового пользователя."""
+        wks = self._get_worksheet("botInfo")
         row = self.get_k_users() + 2
+        
+        # Обновление в одном запросе
+        wks.update_values(
+            f"F{row}:G{row}", 
+            [[str(user_id), flat]]
+        )
 
-        cell_user_id = wks.cell(f"F{row}")
-        cell_user_id.set_value(user_id)
-
-        cell_flat = wks.cell(f"G{row}")
-        cell_flat.set_value(flat)
-
-        return
-
-    # МОЖНО СДЕЛАТЬ АСИНХРОННОЙ?
-    # Выселение пользователей
-    def extraction_agree(self, flat:str):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo") 
-
+    def extraction_agree(self, flat: str) -> None:
+        """Удаляет пользователей квартиры."""
+        wks = self._get_worksheet("botInfo")
         k_users = self.get_k_users()
+        
+        start = self.format_cell("F", 2)
+        end = self.format_cell("G", k_users + 1)
+        
+        data = wks.get_values(start, end)
+        
+        # Фильтрация пользователей
+        filtered_data = [
+            [user_id, user_flat] 
+            for user_id, user_flat in data 
+            if user_flat != flat
+        ]
+        
+        # Дополнение пустыми значениями
+        while len(filtered_data) < k_users:
+            filtered_data.append(["", ""])
+        
+        # Разделение на колонки
+        user_ids = [row[0] for row in filtered_data]
+        flats = [row[1] for row in filtered_data]
+        
+        wks.update_col(6, user_ids, 2)  # Колонка F
+        wks.update_col(7, flats, 2)     # Колонка G
 
-        cell_start = self.Cell("F", 2)
-        cell_end = self.Cell("G", k_users + 1)
+    def get_wks_url(self, sheet_name: str) -> str:
+        """Возвращает URL рабочего листа."""
+        return self._get_worksheet(sheet_name).url
 
-        lst = wks.get_values(cell_start, cell_end)
+    # ----------------- Функции планировщика ----------------- #
 
-        # Создаем новый список пользователей
-        new_users_column = []
-        new_flat_column = []
-        for i in lst:
-            if i[1] != flat:
-                new_users_column.append(i[0])
-                new_flat_column.append(i[1])
+    def get_communal_day(self) -> int:
+        """Возвращает день оплаты коммунальных услуг."""
+        wks = self._get_worksheet("botInfo")
+        return int(wks.get_value(self.format_cell("B", 2)))
 
-        # Добавляем пустые ячейки вместо удаленных записей
-        for i in range(len(lst) - len(new_users_column)):
-            new_users_column.append("")
-            new_flat_column.append("")
+    # ----------------- Форматирование данных ----------------- #
 
-        wks.update_col("F", new_users_column, 1)
-        wks.update_col("G", new_flat_column, 1)
-            
-        return               
-
-    # Получить ссылку на таблицу по имени
-    def get_wks_url(self, sheet_name:str):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, sheet_name) 
-
-        return wks.url
-
-#  - - - - - - - - - -  Функции scheduler - - - - - - - - - - - - - - - - - - - - - #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    
-    # Получить день оплаты коммунальных услуг
-    def get_communal_day(self):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, "botInfo") 
-
-        cell = self.Cell("B", 2)
-        communal_day = int(wks.get_value(cell))
-
-        return communal_day
-
-#  - - - - - - - - - -  Функции форматирования данных - - - - - - - - - - - - - - - #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #    
-
-    # МОЖНО СДЕЛАТЬ АСИНХРОННОЙ?
-    def merge_row(self, string, sheet_name:str):
-        googlesheet_client: pygsheets.client.Client = self._get_googlesheet_client()
-        wks: pygsheets.Spreadsheet = self._get_googlesheet_by_url(googlesheet_client, sheet_name) 
-
+    def merge_row(self, event_type: str, sheet_name: str) -> None:
+        """Объединяет ячейки для события."""
+        wks = self._get_worksheet(sheet_name)
         row = self.get_row_communal(sheet_name) + 1
-
-        cell_start = self.Cell("A", row)
-        cell_end = self.Cell("T", row)
-
-        cell_date = wks.cell(f"A{row}")
-        cell_date.set_value(string)
-
-        rng = wks.get_values(cell_start, cell_end, returnas='range')
-        rng.merge_cells()
-
-        return
+        
+        # Установка значения и объединение
+        wks.cell(f"A{row}").set_value(event_type)
+        wks.merge_cells(
+            f"A{row}", 
+            f"T{row}", 
+            merge_type=pygsheets.MergeType.MERGE_ALL
+        )
